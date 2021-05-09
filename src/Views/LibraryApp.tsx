989a391942ba5e13538f247385d062866735fe00
Manakal <dp.manakal82@gmail.com>
2021-05-09 10:02:41 +0530

    Test Commit with message

--- src/Views/LibraryApp.tsx
+++ src/Views/LibraryApp.tsx
@@ -3,6 +3,7 @@ import WelcomeScreen from "../Components/WelcomeScreen";
 import {Container} from "react-bootstrap";
 import ContentScreen from "../Components/ContentScreen";
 
+//For test purposes
 const LibraryApp:React.FC=()=>{
     return(
         <Container fluid={true}>
